8de1db953a10d713f2972d6add8cc3779b1253b3
Tonny Aleph D. Bezerra <tonnyaleph@outlook.com>
2016-08-03 09:56:32 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,6 @@
+//Criação de objetos na Classe Aluno, direcionado ao arquivo PrimeiraClasse.java
+public class Aluno {
+    public String nome;
+    public double notaMat, 
+                  notaFis;
+}
